d28b60ed34433eab968d9219f5ed15ee35250f4d
Anders Jurisoo <jurisoo@hotmail.com>
2018-12-29 13:45:46 +0800

    Almost at the tensor. Woho!!!

--- src/examples/migration-analysis/02_transform.py
+++ src/examples/migration-analysis/02_transform.py
@@ -2,62 +2,44 @@ import sys
 import os
 from os import walk
 from glob import glob
+import numpy as np
+import re
+
+from MigrationFile import MigrationFile
 
 sys.path.append(os.path.join(os.path.dirname(os.path.realpath(__file__)), "../../"))
-import coach.speak as s #pylint: disable=E0401
 from Print import Print #pylint: disable=E0401
 import Paths #pylint: disable=E0401
 
 print = Print()
 
-def regex_for(type):
-    expressions = {
-        "table": r"(?:Schema::create\(')(.*)(?:')",
-        "column_data_type": r"(?:\$table->)([a-z_A-Z]*)",
-        "column_name": r"(?:\$table->.*')(.*)(?:')",
-    }
-
-    return expressions[type]
-
 def files():
-    files = [y for x in os.walk(Paths.raw) for y in glob(os.path.join(x[0], '*.php'))]
-    return list(map(lambda r: os.path.realpath(r), files))
+    files = [y for x in os.walk(Paths.raw) for y in glob(os.path.join(x[0], '*/*/**/*.php'))]
+    migration_files = list(map(lambda r: MigrationFile(os.path.realpath(r)), files)) 
+    return list(filter(lambda mf: mf.qualifies(), migration_files))
 
-
-
-def table_name(file):
-    return "users"
-
-def column_definitions(file):
-    return [
-			r"$table->increments('id');",
-			r"$table->string('username', 190)->unique();",
-			r"$table->string('password', 60);",
-			r"$table->string('name');",
-			r"$table->string('remember_token', 100)->nullable();",
-			r"$table->timestamps();",
-    ]
-
-def transform(files):
+def transform(migration_files):
     rows = []
-    for file_path in files:
-        parts = file_path.split('/')
-        file = parts[-1]
-        repo = parts[-4]
-        user = parts[-5]
-        for definition in column_definitions(file_path):
+    for migration_file in migration_files:
+        for column_data_type, column_name in migration_file.column_definitions:
             rows.append((
-                    user,
-                    repo,
-                    file,
-                    table_name(definition),
-                    "string",
-                    "some_column_name"
+                    migration_file.user,
+                    migration_file.repo,
+                    migration_file.name,
+                    migration_file.table,
+                    column_data_type,
+                    column_name
             ))
     return rows
 
-for row in transform(files()):
+rows = transform(files())
+
+for row in rows:
     print(row)
+
+#14329 with all qualified #12197 when removed no table guys.
+
+
 
 # Disqualifyers (
 #   multiple tables,
---
+++ src/examples/migration-analysis/MigrationFile.py
@@ -0,0 +1,85 @@
+import os
+import sys
+import base64
+import time
+import datetime
+import re
+import inspect
+from datetime import timedelta
+from github import Github
+
+# Add parent src hack 
+sys.path.append(os.path.join(os.path.dirname(os.path.realpath(__file__)), "../../"))
+
+from Print import Print #pylint: disable=E0401
+from Env import env #pylint: disable=E0401
+
+
+print = Print() # add glorious indentation and colors to print
+
+
+class MigrationFile(object):
+    def __init__(self, path):
+        self.path = path
+        with open(path) as file:
+            self.content = file.read()
+            self.table = self.find_table()
+            self.column_definitions = self.find_column_definitions()
+            self.extract_path_data(path)
+
+    def extract_path_data(self, path):
+            parts = path.split('/')
+            self.name = parts[-1]
+            self.repo = parts[-4]
+            self.user = parts[-5]
+
+    def find_table(self):
+        matches = re.findall(
+                self.regex_for_table,
+                self.content
+        )
+
+        # Dont accept multiple table creations in the same file
+        if len(matches) != 1:
+            return False
+
+        return matches[0]        
+
+    def qualifies(self):
+        return bool(self.table) and True
+
+    def find_column_definitions(self):
+        data_types = re.findall(
+                self.regex_for_column_data_type,
+                self.content
+        )
+
+        names = re.findall(
+                self.regex_for_column_name,
+                self.content
+        )
+
+        if len(data_types) != len(names):
+            return []        
+
+        return zip(data_types, names)
+
+
+    def regex_for(self, type):
+        expressions = {
+            "table": r"(?:Schema::create\(')(.*)(?:')",
+            "column_data_type": r"(?:\$table->)([a-zA-Z1-9_]*)",
+            "column_name": r"(?:\$table->)(?:[a-z_A-Z]*)\('([a-zA-Z1-9_]*)",
+        }
+        return expressions[type]
+
+    def __getattr__(self, name):
+        if name.startswith('regex_for_'):
+            return self.regex_for(
+                    name.split("regex_for_",1)[1] 
+            )
+        raise Exception('No such method')        
+
+if __name__ == '__main__':
+    # Demo of the class 
+    pass
